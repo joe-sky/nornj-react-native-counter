0ab11c0f5c3100b0338b6635a38f5e7c0fc36644
bjzhoutao <zhoutaoinfo@jd.com>
2017-08-15 14:34:00 +0800

    Added styled-components

--- njConfig.js
+++ njConfig.js
@@ -1,7 +1,2 @@
-import { registerComponent } from 'nornj';
 import 'nornj-react/native';
-import { Provider } from 'react-redux';
-
-registerComponent({
-  Provider
-});
+import 'nornj-react/redux';
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,45 +5,144 @@ import React, {
 import { StyleSheet } from 'react-native';
 import { registerTmpl } from 'nornj-react/native';
 import { autobind } from 'core-decorators';
+import styled from 'styled-components/native';
+
+const colors = {
+  background: {
+    bg: '#F5FCFF'
+  },
+  add: {
+    font: '#F69',
+    border: '#F69',
+    bg: '#FFC1D6'
+  },
+  minus: {
+    font: '#6495ED',
+    border: '#6495ED',
+    bg: '#D0DFF9'
+  }
+}
+
+const styles = StyleSheet.create({
+  inline: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  numberBlock: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: 200,
+  },
+  unitBlock: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+  },
+  textColorAdd: {
+    color: colors.add.font,
+  },
+  textColorMinus: {
+    color: colors.minus.font,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
 
 @registerTmpl({
   name: 'Counter',
+  components: {
+    Container: styled.View `
+      flex: 1;
+      justify-content: center;
+      align-items: center;
+      background-color: ${colors.background.bg};
+    `,
+    DisplayPanel: styled.View `
+      margin-vertical: 30px;
+      flex-direction: row;
+      align-items: flex-end;
+    `,
+    ControlPanel: styled.View `
+      margin-vertical: 30px;
+    `,
+    ButtonAddSmall: styled.TouchableHighlight `
+      justify-content: center;
+      align-items: center;
+      height: 30px;
+      margin: 10px;
+      border-radius: 10px;
+      border-width: 2px;
+      border-color: ${colors.add.border};
+      width: 80px;
+    `,
+    ButtonMinusSmall: styled.TouchableHighlight `
+      justify-content: center;
+      align-items: center;
+      height: 30px;
+      margin: 10px;
+      border-radius: 10px;
+      border-width: 2px;
+      border-color: ${colors.minus.border};
+      width: 80px;
+    `,
+    ButtonAdd: styled.TouchableHighlight `
+      justify-content: center;
+      align-items: center;
+      height: 30px;
+      margin: 10px;
+      border-radius: 10px;
+      border-width: 2px;
+      border-color: ${colors.add.border};
+      padding-left: 10px;
+      padding-right: 10px;
+    `,
+    ButtonMinus: styled.TouchableHighlight `
+      justify-content: center;
+      align-items: center;
+      height: 30px;
+      margin: 10px;
+      border-radius: 10px;
+      border-width: 2px;
+      border-color: ${colors.minus.border};
+      padding-left: 10px;
+      padding-right: 10px;
+    `,
+  },
   template: `
-    <View style={styles.container}>
-      <View style={styles.displayPanel}>
+    <Container>
+      <DisplayPanel>
         <Text style={styles.numberBlock}>{counter}</Text>
         <Text style={styles.unitBlock}>/ times</Text>
-      </View>
-      <View style="{list(styles.controlPanel, styles.inline)}">
-        <TouchableHighlight onPress={increment}
-                            style={styles.buttonAddSmall}
-                            underlayColor={colors.add.bg}>
+      </DisplayPanel>
+      <ControlPanel style="{styles.inline}">
+        <ButtonAddSmall onPress={increment}
+                        underlayColor={colors.add.bg}>
           <Text style="{list(styles.text, styles.textColorAdd)}">+</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={decrement}
-                            style={styles.buttonMinusSmall}
-                            underlayColor={colors.minus.bg}>
+        </ButtonAddSmall>
+        <ButtonMinusSmall onPress={decrement}
+                          underlayColor={colors.minus.bg}>
           <Text style="{list(styles.text, styles.textColorMinus)}">-</Text>
-        </TouchableHighlight>
-      </View>
-      <View style={styles.controlPanel}>
-        <TouchableHighlight onPress={incrementIfOdd}
-                            style={styles.buttonAdd}
-                            underlayColor={colors.add.bg}>
+        </ButtonMinusSmall>
+      </ControlPanel>
+      <ControlPanel>
+        <ButtonAdd onPress={incrementIfOdd}
+                   underlayColor={colors.add.bg}>
           <Text style="{list(styles.text, styles.textColorAdd)}">Increment if odd</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={onPressAdd}
-                            style={styles.buttonAdd}
+        </ButtonAdd>
+        <ButtonAdd onPress={onPressAdd}
                             underlayColor={colors.add.bg}>
           <Text style="{list(styles.text, styles.textColorAdd)}">Increment async</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={onPressMinus}
-                            style={styles.buttonMinus}
-                            underlayColor={colors.minus.bg}>
+        </ButtonAdd>
+        <ButtonMinus onPress={onPressMinus}
+                     underlayColor={colors.minus.bg}>
           <Text style="{list(styles.text, styles.textColorMinus)}">Decrement async</Text>
-        </TouchableHighlight>
-      </View>
-    </View>
+        </ButtonMinus>
+      </ControlPanel>
+    </Container>
   `
 })
 class Counter extends Component {
@@ -75,106 +174,5 @@ class Counter extends Component {
     });
   }
 }
-
-const colors = {
-  background: {
-    bg: '#F5FCFF'
-  },
-  add: {
-    font: '#F69',
-    border: '#F69',
-    bg: '#FFC1D6'
-  },
-  minus: {
-    font: '#6495ED',
-    border: '#6495ED',
-    bg: '#D0DFF9'
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.background.bg,
-  },
-  displayPanel: {
-    marginVertical: 30,
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-  },
-  controlPanel: {
-    marginVertical: 30
-  },
-  inline: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-  },
-  numberBlock: {
-    fontSize: 100,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    width: 200,
-  },
-  unitBlock: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'flex-end',
-  },
-  textColorAdd: {
-    color: colors.add.font,
-  },
-  textColorMinus: {
-    color: colors.minus.font,
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  buttonAddSmall: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 30,
-    margin: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: colors.add.border,
-    width: 80,
-  },
-  buttonMinusSmall: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 30,
-    margin: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: colors.minus.border,
-    width: 80,
-  },
-  buttonAdd: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 30,
-    margin: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: colors.add.border,
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  buttonMinus: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 30,
-    margin: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: colors.minus.border,
-    paddingLeft: 10,
-    paddingRight: 10
-  }
-});
 
 export default Counter;
